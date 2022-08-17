14c110f677ea337a82f0ec0f420f4c1ab68af303
abdulraqueeb401 <abdulraqueeb2000@gmail.com>
2022-08-17 17:48:16 +0530

    add a route to get current user

--- routes/users.js
+++ routes/users.js
@@ -3,6 +3,12 @@ const router = express.Router();
 const { User, validateUser } = require("../models/user");
 const bcrypt = require("bcrypt");
 const _ = require("lodash");
+const auth = require("../middleware/auth");
+
+router.get("/me", auth, async (req, res) => {
+    const user = await User.findById(req.user._id).select("-password");
+    res.send(user);
+});
 
 router.post("/", async (req, res) => {
     const { error } = validateUser(req.body);
