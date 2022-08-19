89cb73755a44fdc5a61f918749fe9f040b187544
abdulraqueeb401 <abdulraqueeb2000@gmail.com>
2022-08-19 17:57:08 +0530

    dynamically call db acc. to config

--- startup/db.js
+++ startup/db.js
@@ -1,10 +1,12 @@
 const mongoose = require("mongoose");
 const logger = require("./logging");
+const config = require("config");
 module.exports = function () {
+    const db = config.get("db");
     mongoose
-        .connect("mongodb://localhost/movie_rentals")
+        .connect(db)
         .then(() => {
-            logger.log("info", "connected to mongodb");
+            logger.log("info", `connected to ${db}`);
         })
         .catch((err) => logger.log("error", err.message, err));
 };
