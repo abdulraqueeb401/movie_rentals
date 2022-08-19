43b3c443611148042a64eda349ee547881545ce0
abdulraqueeb401 <abdulraqueeb2000@gmail.com>
2022-08-19 17:45:50 +0530

    correct response stataus to 500

--- middleware/error.js
+++ middleware/error.js
@@ -1,6 +1,6 @@
 const logger = require("../startup/logging");
 module.exports = function (err, req, res, next) {
     logger.error(err.message, err);
-    //501 - internal server error
-    res.status(501).send("something failed");
+    //500 - internal server error
+    res.status(500).send("something failed");
 };
