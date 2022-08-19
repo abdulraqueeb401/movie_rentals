3ebfd4c9329088afb58f4a52b6346568bbd987ec
abdulraqueeb401 <abdulraqueeb2000@gmail.com>
2022-08-19 23:00:47 +0530

    minor bug fix

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/prod")(app);
 
-const port = process.env.PORT | 3000;
+const port = process.env.PORT || 3000;
 const server = app.listen(port, () => {
     logger.log("info", `listening on port ${port}`);
 });
