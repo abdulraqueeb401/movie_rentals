e4120e326af1508aa04577ffca6585c09b063824
abdulraqueeb401 <abdulraqueeb2000@gmail.com>
2022-08-19 18:01:25 +0530

    add unit test to user model

---
+++ tests/unit/models/user.test.js
@@ -0,0 +1,17 @@
+const { User } = require("../../../models/user");
+const jwt = require("jsonwebtoken");
+const config = require("config");
+const mongoose = require("mongoose");
+describe("user.generateAuthToken", () => {
+    it("should return a valid JWT", () => {
+        const payload = {
+            _id: new mongoose.Types.ObjectId().toHexString(),
+            isAdmin: false,
+        };
+        const user = new User(payload);
+        const token = user.generateAuthToken();
+        expect(jwt.verify(token, config.get("jwtPrivateKey"))).toMatchObject(
+            payload
+        );
+    });
+});
